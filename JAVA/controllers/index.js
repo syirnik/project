791acbf536ea0b70e95debf77fb66e0dc9af04b0
syirnik <149399610+syirnik@users.noreply.github.com>
2024-04-15 21:28:58 +0300

    Update index.js

--- JAVA/controllers/index.js
+++ JAVA/controllers/index.js
@@ -1,11 +1,1 @@
-const mainRouteController = require("./main")
-const voteRouteController = require("./vote")
-const defaultRouteController = require("./default")
-const gameRouteController = require("./game")
 
-module.exports = {
-    mainRouteController,
-    voteRouteController,
-    gameRouteController,
-    defaultRouteController
-}
